647a1624c2f0435d0ffa8fa802fb4baf6783bc03
Aaron de los Santos <hansaaron_d@yahoo.com>
2020-02-16 21:03:01 -0500

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -2,6 +2,7 @@ import csv
 import pprint
 
 filepath = "/Users/aarondelossantos/Desktop/Course Syllabus and Schedule/Week 21.csv"
+possibleSchedTypes = ("A", "B", "C", "D", "E", "F", "G")
 
 def extractData(filepath):
     print("processing")
@@ -16,6 +17,16 @@ def extractData(filepath):
 
     return nestedDict
 
+def findScheduleType(scheduleType, dataSource):
+    print("Finding schedule type")
+    siftedDict = {}
+    for key in dataSource:
+        if dataSource[key]["Schedule type"] == scheduleType:
+            siftedDict[key] = dataSource[key]
+    return siftedDict
+
 if __name__ == '__main__':
     extractedData = extractData(filepath)
     pprint.pprint(extractedData[1]["Subject"])
+    scheduleA = findScheduleType("A", extractedData)
+    print(scheduleA)
