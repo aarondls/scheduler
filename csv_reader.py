b2e4356952946dd99bc11a416336c5a2160cea43
Aaron de los Santos <hansaaron_d@yahoo.com>
2020-02-28 23:52:53 -0500

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -5,20 +5,22 @@ filepath = "/Users/aarondelossantos/Desktop/Course Syllabus and Schedule/Full Sc
 possibleSchedTypes = ("A", "B", "C", "D", "E", "F", "G")
 
 def extractData(filepath):
-    print("processing")
+    print("Extracting events from CSV file")
 
-    csvReader = csv.DictReader(open(filepath,mode='r', encoding='utf-8-sig'))
+    try:
+        csvReader = csv.DictReader(open(filepath,mode='r', encoding='utf-8-sig'))
 
-    nestedDict = {}
-    print("preparing nested dict")
+        nestedDict = {}
 
-    for schedType in possibleSchedTypes:
-        nestedDict[schedType] = {}
+        for schedType in possibleSchedTypes:
+            nestedDict[schedType] = {}
 
-    for counter, row in enumerate(csvReader):
-        counter = len(nestedDict[dict(row)["Schedule type"]])
-        nestedDict[dict(row)["Schedule type"]][counter] = dict(row)
-
+        for counter, row in enumerate(csvReader):
+            counter = len(nestedDict[dict(row)["Schedule type"]])
+            nestedDict[dict(row)["Schedule type"]][counter] = dict(row)
+    except:
+        print("Unable to import events")
+        raise
     return nestedDict
 
 extractedData = extractData(filepath)
