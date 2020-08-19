9995995cdfd732d14e8cb01205ca661dd954e3e9
Aaron de los Santos <hansaaron_d@yahoo.com>
2020-08-19 14:45:52 -0700

    upcoming events in primary calendar displayed

--- Schedulebot.py
+++ Schedulebot.py
@@ -38,7 +38,7 @@ service = build('calendar', 'v3', credentials=credentials)
 
 # Call the Calendar API
 now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-print('Getting the upcoming 10 events')
+print('Getting the upcoming 10 events in primary calendar')
 events_result = service.events().list(calendarId='primary', timeMin=now,
                                     maxResults=10, singleEvents=True,
                                     orderBy='startTime').execute()
--- csv_reader.py
+++ csv_reader.py
@@ -1,13 +1,14 @@
 import csv
 import pprint
 
-filepath = "/Users/aarondelossantos/Documents/Schedules/Fall 2020.csv"
+#filepath = "/Users/aarondelossantos/Documents/Schedules/Fall 2020.csv"
+filepath = "/Users/aarondelossantos/Desktop/Course Syllabus and Schedule/Reserve Schedule.csv"
 
 # A to Z type of schedule
-# possibleSchedTypes = ["A", "B", "C", "D", "E", "F", "G"]
+possibleSchedTypes = ["A", "B", "C", "D", "E", "F", "G"]
 
 # M to F type of schedule
-possibleSchedTypes = ["M", "T", "W", "R", "F"]
+# possibleSchedTypes = ["M", "T", "W", "R", "F"]
 
 def extractData(filepath):
     print("Extracting events from CSV file")
